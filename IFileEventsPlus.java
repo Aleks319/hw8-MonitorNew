858c552f3e030ad4004d7d2dfa4bc02d3a7fcab0
Oleksandr Yevtushenko <aleks_319@inbox.ru>
2016-09-28 22:56:17 +0300

    Delete IFileEventsPlus.java

--- IFileEventsPlus.java
+++
@@ -1,8 +0,0 @@
-package lesson8.hw.MonitorNew;
-
-public interface IFileEventsPlus {
-    void onFileAdded(String path);
-    void onFileDeleted(String path);
-
-    void onFileChanged(String path);
-}
